75db8796a2b5a11bb19b92d59b8618fb3dd3d745
yanghy <snxamdf@126.com>
2024-12-17 20:14:54 +0800

    测试功能

--- client.go
+++ client.go
@@ -1,9 +1,10 @@
 package rawhttp
 
 import (
+	"errors"
 	"fmt"
+	"github.com/energye/rawhttp/client"
 	"io"
-	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
@@ -98,10 +99,34 @@ func (c *Client) getConn(protocol, host string, options *Options) (Conn, error) 
 }
 
 func (c *Client) send(method, uri string, headers map[string][]string, body io.Reader, redirectStatus *RedirectStatus, options *Options) (*http.Response, error) {
-	client := &http.Client{
+	protocol := "http"
+	if strings.HasPrefix(strings.ToLower(uri), "https://") {
+		protocol = "https"
+	}
+	var proxyURL *url.URL
+	if options.Proxy != "" {
+		var err error
+		proxyURL, err = url.Parse(options.Proxy)
+		if err != nil {
+			return nil, err
+		}
+	}
+	httpClient := &http.Client{
 		Jar: c.jar,
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			if len(via) > redirectStatus.MaxRedirects {
+				return errors.New(fmt.Sprintf("已达到最大重定向数: %v", redirectStatus.MaxRedirects))
+			}
+			if req.Response != nil {
+				redirectUrl := headerValue(req.Response.Header, "Location")
+				if redirectUrl != "" {
+					return http.ErrUseLastResponse
+				}
+			}
 			return nil
+		},
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
 		},
 	}
 	req, err := http.NewRequest(method, uri, body)
@@ -111,23 +136,41 @@ func (c *Client) send(method, uri string, headers map[string][]string, body io.R
 	for key, val := range headers {
 		req.Header.Set(key, val[0])
 	}
-	resp, err := client.Do(req)
+	host := req.Host
+	if options.AutomaticHostHeader {
+		req.Header.Set("Host", fmt.Sprintf(" %s", host))
+	}
+	req.Header.Set("User-Agent", "energy raw http/1.0.0")
+	req.Header.Set("Accept", "*/*")
+
+	resp, err := httpClient.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+
+	code := resp.StatusCode
+	var isRedirect = func() bool {
+		if code == client.REDIRECTION_NOT_MODIFIED {
+			return false
+		}
+		return code >= client.REDIRECTION_MULTIPLE_CHOICES && code < client.CLIENT_ERROR_BAD_REQUEST
+	}
+	// 301 302 303 307 308
+	if isRedirect() && redirectStatus.FollowRedirects && redirectStatus.Current <= redirectStatus.MaxRedirects {
+		// consume the response body
+		_, err := io.Copy(io.Discard, resp.Body)
+		if err := firstErr(err, resp.Body.Close()); err != nil {
+			return nil, err
+		}
+		redirectUrl := headerValue(resp.Header, "Location")
+		if strings.HasPrefix(redirectUrl, "/") {
+			redirectUrl = fmt.Sprintf("%s://%s%s", protocol, host, redirectUrl)
+		}
+		redirectStatus.Current++
+		return c.send(method, redirectUrl, headers, body, redirectStatus, options)
 	}
 
 	return resp, nil
-	//defer resp.Body.Close()
-	//
-	//for _, cookie := range c.jar.Cookies(resp.Request.URL) {
-	//	fmt.Printf("Cookie: %s=%s\n", cookie.Name, cookie.Value)
-	//}
-	//
-	//bodydata, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println(string(body))
 }
 
 func (c *Client) do(method, uri, uriPath string, headers map[string][]string, body io.Reader, redirectStatus *RedirectStatus, options *Options) (*http.Response, error) {
@@ -142,7 +185,6 @@ func (c *Client) do(method, uri, uriPath string, headers map[string][]string, bo
 	if err != nil {
 		return nil, err
 	}
-
 	host := u.Host
 	if options.AutomaticHostHeader {
 		// add automatic space
--- rawhttp_test.go
+++ rawhttp_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestGetSource(t *testing.T) {
-	url := "https://energy.yanghy.cn/energye/liblcl/releases/download/v2.3.7/liblcl.Windows64.zip"
+	url := "https://sourceforge.net/projects/liblcl/files/v2.3.8/liblcl-109.Windows64.zip"
 	client := NewClient(DefaultOptions)
 	resp, err := client.Get(url)
 	if err != nil {
@@ -31,8 +31,8 @@ func TestGetTxtSource(t *testing.T) {
 	bufRead(resp, "md5.txt")
 }
 
-func _TestProxtGetTxtSource(t *testing.T) {
-	url := "https://sourceforge.net/projects/liblcl/files/v2.3.7/md5.txt"
+func _TestProxyGetSource(t *testing.T) {
+	url := "https://sourceforge.net/projects/liblcl/files/v2.3.8/liblcl-109.Windows64.zip"
 	options := *DefaultOptions
 	options.Proxy = "http://127.0.0.1:10809"
 	client := NewClient(&options)
@@ -41,7 +41,7 @@ func _TestProxtGetTxtSource(t *testing.T) {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	bufRead(resp, "md5.txt")
+	bufRead(resp, "liblcl-109.Windows64.zip")
 }
 
 func bufRead(resp *http.Response, saveName string) {
